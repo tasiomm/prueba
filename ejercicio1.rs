27f417877924503764c3bee14c647ee767aa457e
tasiomm <tasiomateos.informatica@iespacomolla.es>
2023-03-17 09:22:38 +0100

    Añado archivo de rust

---
+++ ejercicio1.rs
@@ -0,0 +1,29 @@
+// Definición de la estructura
+struct Persona {
+    nombre: String,
+    edad: u32,
+}
+
+// Implementación de métodos para la estructura
+impl Persona {
+    // Método constructor
+    fn new(nombre: &str, edad: u32) -> Self {
+        Persona {
+            nombre: nombre.to_string(),
+            edad,
+        }
+    }
+
+    // Método para mostrar información sobre la persona
+    fn presentarse(&self) {
+        println!("Hola, mi nombre es {} y tengo {} años.", self.nombre, self.edad);
+    }
+}
+
+fn main() {
+    // Crear una instancia de Persona
+    let persona1 = Persona::new("Juan", 30);
+
+    // Llamar al método presentarse
+    persona1.presentarse();
+}
